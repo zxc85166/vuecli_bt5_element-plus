07a1a7f7f10b361583372f4632215668ea6556c6
zxc85166 <jimmya011@gmail.com>
2021-06-14 21:35:01 +0800

    https://www.youtube.com/watch?v=L77Uq93XXzk

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,41 @@ export default {
 <template>
   <div id="app">
     <Header />
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition
+        enter-active-class="animate__animated animate__fadeInLeft"
+        leave-active-class="animate__animated animate__fadeOutLeft"
+        mode="out-in"
+      >
+        <component :is="Component" />
+      </transition>
+    </router-view>
     <Footer />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
   text-decoration: none;
+  a:hover,
+  a.router-link-active {
+    border-bottom: 2px solid #3498db;
+  }
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s ease-out;
+  }
+  // .page {
+  //   position: absolute;
+  //   top: 30px;
+  // }
 }
 </style>
