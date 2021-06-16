1ce7d8cac0ba1c247810b3bc6e3642e856d175fc
zxc85166 <jimmya011@gmail.com>
2021-06-16 16:52:37 +0800

    .

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,22 +35,22 @@ export default {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link class="mx-4" to="/"> 首頁</router-link>
+              <router-link class="mx-3" to="/"> 首頁</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="mx-4" to="/users"> 我的看診流程</router-link>
+              <router-link class="mx-3" to="/users"> 我的看診流程</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="mx-4" to="/page2"> 我該看哪一科</router-link>
+              <router-link class="mx-3" to="/page2"> 我該看哪一科</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="mx-4" to="/page3"> 復康巴士申請</router-link>
+              <router-link class="mx-3" to="/page3"> 復康巴士申請</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="mx-4" to="/page4"> 線上填表</router-link>
+              <router-link class="mx-3" to="/page4"> 線上填表</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="mx-4" to="/page5"> 用藥追蹤</router-link>
+              <router-link class="mx-3" to="/page5"> 用藥追蹤</router-link>
             </li>
           </ul>
         </div>
