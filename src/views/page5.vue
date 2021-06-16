1cb4b88a625955e7711e90e929029cbb078f796f
zxc85166 <jimmya011@gmail.com>
2021-06-16 12:11:10 +0800

    .

--- src/views/page5.vue
+++ src/views/page5.vue
@@ -126,4 +126,10 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.table-hover {
+  > tbody > tr:hover {
+    font-weight: bold;
+  }
+}
+</style>
